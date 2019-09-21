ea3bbc5bc0698ad7111bcaec6cbffe648b138aa4
Pedro Almeida <pedro.almeida@saqueepague.com.br>
2019-09-21 20:46:46 -0300

    Corrigindo unit test

--- src/module-info.java
+++
@@ -1,6 +0,0 @@
-module atividadeProgramacao {
-	exports controller;
-	exports main;
-	exports model;
-	requires org.junit.jupiter.api;
-}
--- test/model/AnalistaTest.java
+++ test/model/AnalistaTest.java
@@ -17,6 +17,7 @@ class AnalistaTest {
 	@BeforeEach
 	void CreateAnalista() {
 		Analista analista = new Analista("pedro", "00684932055", "prvalmeida", "123", 123456);
+		this.analista = analista;
 	}
 
 	@Test
@@ -26,19 +27,18 @@ class AnalistaTest {
 
 	@Test
 	void testAdicionaProposta() {
-//TODO corrigir exceção
-//		LocalDate dataNascimentoCliente = LocalDate.of(1986, 11, 14);
-//		Cliente cliente = new Cliente ("joao", 'm', dataNascimentoCliente, "0064932055", "maria");
-//		LocalDate dataEmissao = LocalDate.of(2019, 9, 21);
-//		
-//		String idProposta = this.analista.adicionaProposta(3000, cliente, dataEmissao, "inss", "123456");
-//		
-//		ArrayList<Proposta> propostas = (ArrayList) this.analista.getPropostas();
-//    	for (Proposta p : propostas) {
-//    		if (p.getId() == idProposta) {
-//    			assertEquals(p.getCliente(), cliente);
-//    		}
-//    	}
+		LocalDate dataNascimentoCliente = LocalDate.of(1986, 11, 14);
+		Cliente cliente = new Cliente ("joao", 'm', dataNascimentoCliente, "0064932055", "maria");
+		LocalDate dataEmissao = LocalDate.of(2019, 9, 21);
+		
+		String idProposta = this.analista.adicionaProposta(3000, cliente, dataEmissao, "inss", "123456");
+		
+		ArrayList<Proposta> propostas = (ArrayList) this.analista.getPropostas();
+    	for (Proposta p : propostas) {
+    		if (p.getId() == idProposta) {
+    			assertEquals(p.getCliente(), cliente);
+    		}
+    	}
 	}
 
 	@Test
